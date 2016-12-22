e135f038f115206ad042242832573d34164ab329
Karlzone <Karlzone0@gmail.com>
2016-12-23 00:22:30 +0100

    Stuff

--- FileCopyAutomater/AddNewWindow.xaml.cs
+++ FileCopyAutomater/AddNewWindow.xaml.cs
@@ -28,20 +28,67 @@ namespace FileCopyAutomater
             checkBox_CanOverwrite.IsChecked = false;
         }
         private MainWindow _mainWindow;
+        private bool _multipleItemsSelected = false;
+        private string[] _multiSelectedItems;
 
         private void button_Browse1_Click(object sender, RoutedEventArgs e)
         {
+            string[] pathArray = BrowseFiles(true);
 
+            if (pathArray.Length > 1)
+            {
+                var text = new StringBuilder();
+                text.Append(pathArray.First());
+                for (int i = 1; i < pathArray.Length; i++)
+                {
+                    text.Append(", " + pathArray[i]);
+                }
+                textBox_Source.Text = text.ToString();
+                _multipleItemsSelected = true;
+                _multiSelectedItems = pathArray;
+            }
+            else
+            {
+                textBox_Source.Text = pathArray.First();
+                _multipleItemsSelected = false;
+            }
         }
 
         private void button_Browse2_Click(object sender, RoutedEventArgs e)
         {
+            textBox_Target.Text = BrowseFiles(false).First();
+        }
 
+        private string[] BrowseFiles(bool allowMultiSelect)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.CheckFileExists = true;
+            dlg.CheckPathExists = true;
+            dlg.Multiselect = allowMultiSelect;
+            bool? result = dlg.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                return dlg.FileNames;
+            }
+            else
+            {
+                return new string[0];
+            }
         }
 
         private void button_Add_Click(object sender, RoutedEventArgs e)
         {
-            _mainWindow.AllFiles.Add(new SyncData(textBox_Source.Text, textBox_Target.Text, checkBox_CanOverwrite.IsChecked.Value));
+            if (_multipleItemsSelected)
+            {
+                foreach (string file in _multiSelectedItems)
+                {
+                    _mainWindow.AllFiles.Add(new SyncData(file, textBox_Target.Text, checkBox_CanOverwrite.IsChecked.Value));
+                }
+            }
+            else
+            {
+                _mainWindow.AllFiles.Add(new SyncData(textBox_Source.Text, textBox_Target.Text, checkBox_CanOverwrite.IsChecked.Value));
+            }
         }
 
         private void button_Cancel_Click(object sender, RoutedEventArgs e)
